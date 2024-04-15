650c32b88685a938a6105a1c10abb018a8ac41e5
chetra-seng <dev@chetraseng.com>
2024-04-15 14:15:56 +0700

    Added prettier formatter to conform

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,17 @@
+return {
+  "conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      ["javascript"] = { "prettier" },
+      ["javascriptreact"] = { "prettier" },
+      ["typescript"] = { "prettier" },
+      ["typescriptreact"] = { "prettier" },
+      ["vue"] = { "prettier" },
+      ["css"] = { "prettier" },
+      ["scss"] = { "prettier" },
+      ["less"] = { "prettier" },
+      ["html"] = { "prettier" },
+      ["json"] = { "prettier" },
+    },
+  },
+}
