864dcf9c388a577f8512982fcd5cb412969e5ef9
Chetra Seng <dev@chetraseng.com>
2024-05-23 13:18:31 +0700

    Improved linting configuration
    Added eslint configuration with nvim-lint for
    - Javascript
    - Typesciprt
    - React

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -3,6 +3,10 @@ return {
   opts = {
     linters_by_ft = {
       beancount = { "bean_check" },
+      javascript = { "eslint" },
+      typescript = { "eslint" },
+      typescriptreact = { "eslint" },
+      javascriptreact = { "eslint" },
     },
   },
 }
