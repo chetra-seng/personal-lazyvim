be4cf46f3b5b573e59dd93c0d474c88f8495211a
chetra-seng <dev@chetraseng.com>
2024-07-15 15:52:27 +0700

    Added custom keymap icons

---
+++ lua/plugins/whick-key.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/which-key.nvim",
+  opts = {
+    spec = {
+      { "<leader>t", group = "Terminal" },
+      { "<leader>o", group = "Obsidian", icon = {
+        icon = "󱞂",
+        color = "purple",
+      } },
+      { "<leader>j", group = "Java", icon = {
+        icon = "",
+        color = "red",
+      } },
+    },
+  },
+}
