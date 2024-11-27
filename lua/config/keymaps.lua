ace63b0d8257da5f61aa3e103aa6468167a66605
chetra-seng <dev@chetraseng.com>
2024-11-27 10:25:31 +0700

    Updated configuration
    - Disable bufferline plugin
    - Replace oil.nvim with neotree
    - Added oil.nvim git status plugin
    - Disable flash.nvim mode search by default

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -15,3 +15,4 @@ vim.keymap.set("n", "<A-->", "<cmd>resize -2<cr>", { desc = "Decrease Window Hei
 vim.keymap.set("n", "<A-=>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 vim.keymap.set("n", "<A-<>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 vim.keymap.set("n", "<A->>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+vim.keymap.set("n", "-", "<cmd>Oil<cr>", { desc = "Open parent directory" })
--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -1,23 +1,4 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ["<leader>p"] = {
-            "image_wezterm",
-            desc = "Explorer preview image",
-          },
-        },
-      },
-      commands = {
-        image_wezterm = function(state)
-          local node = state.tree:get_node()
-          if node.type == "file" then
-            require("image_preview").PreviewImage(node.path)
-          end
-        end,
-      },
-    },
-  },
+  enabled = false,
 }
