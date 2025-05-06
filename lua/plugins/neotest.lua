4efa81be57f5bc28d40317e3aa50014e888618ae
Chetra Seng <dev@chetraseng.com>
2025-05-07 00:24:58 +0700

    Fixed neotest adapter config

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,14 +1,16 @@
 return {
   "nvim-neotest/neotest",
   dependencies = {
-
     { "marilari88/neotest-vitest" },
     { "thenbe/neotest-playwright" },
+    { "fredrikaverpil/neotest-golang" },
   },
-  opts = { adapters = { "neotest-vitest", "neotest-playwright" } },
+  opts = { adapters = { "neotest-vitest", "neotest-playwright", "neotest-golang" } },
   config = function()
+    local neotest_golang_opts = {} -- Specify custom configuration
     require("neotest").setup({
       adapters = {
+        require("neotest-golang")(neotest_golang_opts), -- Registration
         require("neotest-playwright").adapter({
           options = {
             persist_project_selection = true,
