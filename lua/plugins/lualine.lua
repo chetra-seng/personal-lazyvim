9c8edd78a647682a64d3d90946c25ebfc18ad1eb
Chetra Seng <dev@chetraseng.com>
2025-06-19 17:52:07 +0700

    updated config
    Added git icon to lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,9 +2,11 @@ return {
   "nvim-lualine/lualine.nvim",
   opts = {
     options = {
-
       section_separators = { left = "", right = "" },
       component_separators = { left = "", right = "" },
+    },
+    sections = {
+      lualine_b = { "branch", "diff" },
     },
   },
 }
