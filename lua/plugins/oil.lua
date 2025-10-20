c3b089501db8ea48817e76efe6d94e77d699bec2
Chetra Seng <dev@chetraseng.com>
2025-10-20 23:56:07 +0700

    updated config
    - fixed oil nvim misconfig
    - added copy for oil both linux and mac

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -81,25 +81,25 @@ return {
         is_always_hidden = function(name, bufnr)
           return name == ".."
         end,
-        float = {
-          -- Padding around the floating window
-          padding = 2,
-          max_width = 0,
-          max_height = 0,
-          border = "rounded",
-          win_options = {
-            winblend = 0,
-          },
-          -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
-          get_win_title = nil,
-          -- preview_split: Split direction: "auto", "left", "right", "above", "below".
-          preview_split = "auto",
-          -- This is the config that will be passed to nvim_open_win.
-          -- Change values here to customize the layout
-          override = function(conf)
-            return conf
-          end,
+      },
+      float = {
+        -- Padding around the floating window
+        padding = 2,
+        max_width = 0,
+        max_height = 0,
+        border = "rounded",
+        win_options = {
+          winblend = 0,
         },
+        -- optionally override the oil buffers window title with custom function: fun(winid: integer): string
+        get_win_title = nil,
+        -- preview_split: Split direction: "auto", "left", "right", "above", "below".
+        preview_split = "auto",
+        -- This is the config that will be passed to nvim_open_win.
+        -- Change values here to customize the layout
+        override = function(conf)
+          return conf
+        end,
       },
     })
 
